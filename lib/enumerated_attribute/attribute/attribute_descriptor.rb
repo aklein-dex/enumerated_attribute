dde11ed5c8b409c09e045cb38a40cc67314c6740
Alexandre Klein <alex@dreamexchange.co.jp>
2012-07-05 15:22:29 +0900

    add 'localise' option to translate labels

--- lib/enumerated_attribute/attribute/attribute_descriptor.rb
+++ lib/enumerated_attribute/attribute/attribute_descriptor.rb
@@ -9,7 +9,11 @@ module EnumeratedAttribute
 				super enums
 				@name = name
 				@options = opts
-				@labels_hash = Hash[*self.collect{|e| [e, e.to_s.gsub(/_/, ' ').squeeze(' ').capitalize]}.flatten]
+				if @options.key?(:localize) && @options[:localize]
+					@labels_hash = Hash[*self.collect{|e| [e, e.to_s]}.flatten]
+				else
+					@labels_hash = Hash[*self.collect{|e| [e, e.to_s.gsub(/_/, ' ').squeeze(' ').capitalize]}.flatten]
+				end
 			end
 			
 			def allows_nil?
@@ -23,18 +27,25 @@ module EnumeratedAttribute
 				self
 			end
 			def label(value)
-				@labels_hash[value]
+				if @options.key?(:localize) && @options[:localize]
+					I18n.t(@labels_hash[value])
+				else
+					@labels_hash[value]
+				end
 			end
 			def labels
-				@labels_array ||= self.map{|e| @labels_hash[e]}
+				@labels_array ||= self.map{|e| label(e)}
 			end
 			def hash
 				@labels_hash
 			end
 			def select_options
-				@select_options ||= self.map{|e| [@labels_hash[e], e.to_s]}
+				if @options.key?(:localize) && @options[:localize]
+					@select_options ||= self.map{|e| [I18n.t(@labels_hash[e]), e.to_s]}
+				else
+					@select_options ||= self.map{|e| [@labels_hash[e], e.to_s]}
+				end
 			end
-			
 			def set_label(enum_value, label_string)
 				reset_labels
 				@labels_hash[enum_value.to_sym] = label_string
